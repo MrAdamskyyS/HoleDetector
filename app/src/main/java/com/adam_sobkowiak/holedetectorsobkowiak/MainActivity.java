a34ae7f3921dbbac99612c7df01e0f689e476bd0
Adam Sobkowiak <Adamsky_biznes@o2.pl>
2023-01-05 12:02:12 +0100

    Jo powinno być gut gut

--- app/src/main/java/com/adam_sobkowiak/holedetectorsobkowiak/MainActivity.java
+++ app/src/main/java/com/adam_sobkowiak/holedetectorsobkowiak/MainActivity.java
@@ -1,132 +1,144 @@
 package com.adam_sobkowiak.holedetectorsobkowiak;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
+import java.util.Date;
 
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-    private Sensor gyroscope;
+public class MainActivity extends Activity {
+
     private LocationManager locationManager;
-    private SQLiteDatabase database;
+    private LocationListener locationListener;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
+    private Location currentLocation;
+
+    private TextView text_DD;
+    private TextView text_OD;
+    private TextView text_OD1;
+    private TextView text_OD2;
+    private TextView text_OD3;
+    private TextView text_OD4;
+    private TextView text_OD5;
+    private TextView text_OD6;
+    private TextView text_OD7;
+
+    private Button button_DD;
+
+    private Switch switch1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicjalizuj menedżer sensory
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        text_DD = findViewById(R.id.text_DD);
+        text_OD = findViewById(R.id.text_OD);
+        text_OD1 = findViewById(R.id.text_OD1);
+        text_OD2 = findViewById(R.id.text_OD2);
+        text_OD3 = findViewById(R.id.text_OD3);
+        text_OD4 = findViewById(R.id.text_OD4);
+        text_OD5 = findViewById(R.id.text_OD5);
+        text_OD6 = findViewById(R.id.text_OD6);
+        text_OD7 = findViewById(R.id.text_OD7);
 
-        // Inicjalizuj menedżer lokalizacji
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // zapisz aktualną lokalizację do zmiennej currentLocation
+                currentLocation = location;
+                // pokaż button pozwalający na zapisanie lokalizacji do bazy danych
+                button_DD.setVisibility(View.VISIBLE);
+            }
 
-        // Otwórz połączenie z bazą danych
-        database = openOrCreateDatabase("Potholes", MODE_PRIVATE, null);
-        // Wykonaj zapytanie do bazy danych, aby utworzyć tabelę "potholes"
-        String sql = "CREATE TABLE IF NOT EXISTS potholes (latitude REAL, longitude REAL, depth REAL)";
-        database.execSQL(sql);
-        // Zamknij połączenie z bazą danych
-        database.close();
-    }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Rejestruj nasłuchiwanie zdarzeń z akcelerometru i żyroskopu
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
-        // Rejestruj nasłuchiwanie zdarzeń z lokalizacji
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // pobierz switch z layoutu
+        switch1 = findViewById(R.id.switch1);
+        // ustaw słuchacz dla switcha
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // sprawdź czy użytkownik wyraził zgodę na uzyskiwanie dostępu do lokalizacji
+                    if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // brak zgody, wyświetl komunikat i zakończ działanie aplikacji
+                        Toast.makeText(MainActivity.this, "Brak uprawnień do pobierania lokalizacji", Toast.LENGTH_SHORT).show();
+                        switch1.setChecked(false);
+                        return;
+                    }
+                    // pobieraj aktualną lokalizację co MIN_TIME milisekund, jeśli ruch użytkownika przekroczy MIN_DISTANCE metrów
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener);
+                } else {
+                    // zatrzymaj pobieranie aktualnej lokalizacji
+                    locationManager.removeUpdates(locationListener);
+                }
+            }
+        });
+
+    // pobierz button z layoutu
+    button_DD = findViewById(R.id.button_DD);
+    // ustaw słuchacz dla buttona
+    button_DD.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // sprawdź czy currentLocation zawiera dane o aktualnej lokalizacji
+            if (currentLocation != null) {
+                // zapisz aktualną lokalizację do bazy danych
+                saveLocationToDB(currentLocation);
+                // ukryj button
+                button_DD.setVisibility(View.GONE);
+                // wyświetl komunikat o sukcesie
+                Toast.makeText(MainActivity.this, "Lokalizacja zapisana do bazy danych", Toast.LENGTH_SHORT).show();
+            }
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Wstrzymaj nasłuchiwanie zdarzeń z akcelerometru i żyroskopu
-        sensorManager.unregisterListener(this);
-        // Wstrzymaj nasłuchiwanie zdarzeń z lokalizacji
-        locationManager.removeUpdates(this);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        // Pobierz dane z akcelerometru lub żyroskopu
-        float[] data = event.values;
-        // Przetwarzaj dane, aby wykryć dziury w drodze i pomierzyć ich głębokość
-        // tutaj możesz dodać swój własny kod do analizy danych z akcelerometru lub żyroskopu
-        // i wykrywania dziur w drodze
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        // Pobierz aktualną lokalizację z GPS
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        // Przetwarzaj dane z GPS, aby wykryć dziury w drodze i pomierzyć ich głębokość
-        // tutaj możesz dodać swój własny kod do analizy danych z GPS i wykrywania dziur w drodze
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // Obsługa zmiany statusu lokalizacji
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        // Obsługa włączenia dostawcy lokalizacji
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        // Obsługa wyłączenia dostawcy lokalizacji
-    }
-
-    // Funkcja do zapisywania informacji o dziurach w bazie danych
-    private void savePothole(double latitude, double longitude, double depth) {
-        // Otwórz połączenie z bazą danych
-        database = openOrCreateDatabase("Potholes", MODE_PRIVATE, null);
-        // Wykonaj zapytanie do bazy danych, aby zapisać informacje o dziurze
-        String sql = "INSERT INTO potholes (latitude, longitude, depth) VALUES (?, ?, ?)";
-        SQLiteStatement statement = database.compileStatement(sql);
-        statement.bindDouble(1, latitude);
-        statement.bindDouble(2, longitude);
-        statement.bindDouble(3, depth);
-        statement.execute();
-        // Zamknij połączenie z bazą danych
-        database.close();
-    }
+    });
 }
+
+
+    private void saveLocationToDB(Location location) {
+        // otwórz połączenie z bazą danych
+        SQLiteDatabase db = openOrCreateDatabase("locations", MODE_PRIVATE, null);
+        // utwórz tabelę, jeśli nie istnieje
+        db.execSQL("CREATE TABLE IF NOT EXISTS locations (timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, latitude REAL, longitude REAL);");
+        // utwórz zapytanie INSERT
+        String sql = "INSERT INTO locations (latitude, longitude) VALUES (?, ?)";
+        text_DD.setText("Dodano dziure");
+        // utwórz obiekt zapytania
+        SQLiteStatement statement = db.compileStatement(sql);
+        // ustaw wartości dla zapytania
+        statement.bindDouble(1, location.getLatitude());
+        statement.bindDouble(2, location.getLongitude());
+        // wykonaj zapytanie
+        statement.executeInsert();
+        // zamknij obiekt zapytania i połączenie z bazą danych
+        statement.close();
+        db.close();
+    }
+};
